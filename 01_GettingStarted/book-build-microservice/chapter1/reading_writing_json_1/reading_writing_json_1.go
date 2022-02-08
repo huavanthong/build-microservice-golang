f19cfda95b10495c2a7f7a46bc17bd0a12136c1f
Thong Hua <huavanthong14@gmail.com>
2022-02-08 23:43:23 +0700

    [Chapter1][JSON] Update comments for using message response.

--- 01_GettingStarted/book-build-microservice/chapter1/reading_writing_json_1/reading_writing_json_1.go
+++ 01_GettingStarted/book-build-microservice/chapter1/reading_writing_json_1/reading_writing_json_1.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 )
 
+// Declare response message like message response in Java.
 type helloWorldResponse struct {
 	Message string
 }
@@ -17,10 +18,13 @@ func main() {
 	http.HandleFunc("/helloworld", helloWorldHandler)
 
 	log.Printf("Server starting on port %v\n", port)
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	// Create a response with message following type struct.
+	// Assign string "Hello World" to Message - member of struct.
 	response := helloWorldResponse{Message: "Hello World"}
 	data, err := json.Marshal(response)
 	if err != nil {
