903076e4f9f76d1443672adb955101553b209f6c
Thong Hua <huavanthong14@gmail.com>
2022-02-08 23:59:28 +0700

    [AdvantageCase] Create multiple json message, and check result

--- 01_GettingStarted/book-build-microservice/chapter1/reading_writing_json_2/reading_writing_json_2.go
+++ 01_GettingStarted/book-build-microservice/chapter1/reading_writing_json_2/reading_writing_json_2.go
@@ -27,17 +27,47 @@ func main() {
 	port := 8080
 
 	http.HandleFunc("/helloworld", helloWorldHandler)
+	http.HandleFunc("/helloworld2", helloWorldHandlerAdvantage)
 
 	log.Printf("Server starting on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
+// Normal case: Send one message
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+
 	response := helloWorldResponse{Message: "HelloWorld"}
+
 	data, err := json.Marshal(response)
 	if err != nil {
 		panic("Ooops")
 	}
 
 	fmt.Fprint(w, string(data))
+}
+
+// Advantage case: Send multiple message.
+/*
+Prolem:
+	Đặt vấn đề rằng, nếu chúng ta gửi mutiple json message vào một response. Thì điều gì sẽ xảy ra?
+	Và làm cách nào ta có thể handle được multiple message đó.
+Output:
+
+Solution
+
+*/
+func helloWorldHandlerAdvantage(w http.ResponseWriter, r *http.Request) {
+
+	response1 := helloWorldResponse{Message: "HelloWorld"}
+
+	response2 := helloWorldResponse{Message: "Hello World", Id: 333}
+
+	data1, err := json.Marshal(response1)
+	data2, err := json.Marshal(response2)
+
+	if err != nil {
+		panic("Ooops")
+	}
+
+	fmt.Fprintf(w, string(data1), string(data2))
 }
