be07e29def493da6051ba9c8bc96aa4bfb6007ac
Thong Hua <huavanthong14@gmail.com>
2022-04-12 21:45:18 +0700

    [Handler][Data] Step 4: Create method to encode data json from product, and implement handler for product

--- microservice/product-api/data/products.go
+++ microservice/product-api/data/products.go
@@ -1,5 +1,11 @@
 package data
 
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
 // Product defines the structure for an API product
 type Product struct {
 	ID          int     `json:"id`
@@ -14,6 +20,17 @@ type Product struct {
 
 // Products is a collection of Product
 type Products []*Product
+
+// ToJSON serializes the contents of the collection to JSON
+// NewEncoder provides better performance than json.Unmarshal as it does not
+// have to buffer the output into an in memory slice of bytes
+// this reduces allocations and the overheads of the service
+//
+// https://golang.org/pkg/encoding/json/#NewEncoder
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
 
 // GetProducts returns a list of products
 func GetProducts() Products {
