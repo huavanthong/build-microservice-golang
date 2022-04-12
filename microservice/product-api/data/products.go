b76d40d43d0b165629a15b0a1f09fe961b0695ff
Thong Hua <huavanthong14@gmail.com>
2022-04-12 21:42:14 +0700

    [Data] Step 2: Create a storage, and declare data for product

--- microservice/product-api/data/products.go
+++ microservice/product-api/data/products.go
@@ -12,3 +12,33 @@ type Product struct {
 	DeletedOn   string  `json:"-"`
 }
 
+// Products is a collection of Product
+type Products []*Product
+
+// GetProducts returns a list of products
+func GetProducts() Products {
+	return productList
+}
+
+// productList is a hard coded list of products for this
+// example data source
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
