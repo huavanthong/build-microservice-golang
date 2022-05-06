cebeea4a5427ecceeb01481cc97c25ca73f7fc64
huavanthong <50081052+huavanthong@users.noreply.github.com>
2022-05-06 14:11:10 +0700

    [Client][Encapsulate] Encpsulate client side, and expose API

--- 03_ReferenceProject/03_bookshop/client/client.go
+++ 03_ReferenceProject/03_bookshop/client/client.go
@@ -4,22 +4,44 @@ import (
 	pb "bookshop/server/pb/inventory"
 	"context"
 	"log"
+	"net/rpc"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func main() {
-	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+func CreateClient() *grpc.ClientConn {
+	// Dial creates a client connection to the given target
+	client, err := rpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("failed to connect: %v", err)
+		log.Fatal("failed to connect %v", err)
 	}
-	defer conn.Close()
+	return client
+}
+
+func PerformGetBookList(conn *grpc.ClientConn) pb.GetBookListResponse {
 
 	client := pb.NewInventoryClient(conn)
+
 	bookList, err := client.GetBookList(context.Background(), &pb.GetBookListRequest{})
 	if err != nil {
 		log.Fatalf("failed to get book list: %v", err)
 	}
 	log.Printf("book list: %v", bookList)
+	return bookList
 }
+
+// func main() {
+// 	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+// 	if err != nil {
+// 		log.Fatalf("failed to connect: %v", err)
+// 	}
+// 	defer conn.Close()
+
+// 	client := pb.NewInventoryClient(conn)
+// 	bookList, err := client.GetBookList(context.Background(), &pb.GetBookListRequest{})
+// 	if err != nil {
+// 		log.Fatalf("failed to get book list: %v", err)
+// 	}
+// 	log.Printf("book list: %v", bookList)
+// }
