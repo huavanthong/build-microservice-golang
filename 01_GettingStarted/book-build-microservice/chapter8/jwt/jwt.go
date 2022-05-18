f733b21dcfa8e9561c365d5d74e5f4bbbe876423
hvthong <huavanthong14@gmail.com>
2022-05-18 11:07:22 +0700

    [JWT] Update comment for token.

--- 01_GettingStarted/book-build-microservice/chapter8/jwt/jwt.go
+++ 01_GettingStarted/book-build-microservice/chapter8/jwt/jwt.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/SermoDigital/jose/crypto"
 	"github.com/SermoDigital/jose/jws"
-	bgocrypto "github.com/building-microservices-with-go/crypto"
 )
 
 var rsaPrivate *rsa.PrivateKey
@@ -29,13 +28,19 @@ func init() {
 
 // GenerateJWT creates a new JWT and signs it with the private key
 func GenerateJWT() []byte {
+	// init object to claim a jwt
 	claims := jws.Claims{}
+	// set expire for our token
 	claims.SetExpiration(time.Now().Add(2880 * time.Minute))
+	// set user info
 	claims.Set("userID", "abcsd232jfjf")
 	claims.Set("accessLevel", "user")
 
+	// NewJWT: creates a new JWT with the given claims.
+	// generate jwt token with cryto
 	jwt := jws.NewJWT(claims, crypto.SigningMethodRS256)
 
+	// Serialize helps implements jwt.JWT.
 	b, _ := jwt.Serialize(rsaPrivate)
 
 	return b
@@ -44,11 +49,13 @@ func GenerateJWT() []byte {
 // ValidateJWT validates that the given slice is a valid JWT and the signature matches
 // the public key
 func ValidateJWT(token []byte) error {
+	// parse a input token
 	jwt, err := jws.ParseJWT(token)
 	if err != nil {
 		return fmt.Errorf("Unable to parse token: %v", err)
 	}
 
+	// validate signature from token.
 	if err = jwt.Validate(rsaPublic, crypto.SigningMethodRS256); err != nil {
 		return fmt.Errorf("Unable to validate token: %v", err)
 	}
