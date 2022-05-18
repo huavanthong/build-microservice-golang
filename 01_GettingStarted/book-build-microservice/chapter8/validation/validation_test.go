9e229316f6d43b2a39847d3fc605298a04dff23a
hvthong <huavanthong14@gmail.com>
2022-05-18 11:48:21 +0700

    [Validation][Testing] Add more test case for testing.

--- 01_GettingStarted/book-build-microservice/chapter8/validation/validation_test.go
+++ 01_GettingStarted/book-build-microservice/chapter8/validation/validation_test.go
@@ -6,17 +6,20 @@ import (
 	"gopkg.in/go-playground/validator.v9"
 )
 
+// Case 1: Test return error if a email is not exist in a request
 func TestErrorWhenRequestEmailNotPresent(t *testing.T) {
 	validate := validator.New()
 	request := Request{
 		URL: "http://nicholasjackson.io",
 	}
 
+	// err == nil: validate return error
 	if err := validate.Struct(&request); err == nil {
 		t.Error("Should have raised an error")
 	}
 }
 
+// Case 2: Test return error if a email is invalid
 func TestErrorWhenRequestEmailIsInvalid(t *testing.T) {
 	validate := validator.New()
 	request := Request{
@@ -24,11 +27,13 @@ func TestErrorWhenRequestEmailIsInvalid(t *testing.T) {
 		URL:   "http://nicholasjackson.io",
 	}
 
+	// err == nil: validate return error
 	if err := validate.Struct(&request); err == nil {
 		t.Error("Should have raised an error")
 	}
 }
 
+// Case 3: Test return success if a name is not present
 func TestNoErrorWhenRequestNameNotPresent(t *testing.T) {
 	validate := validator.New()
 	request := Request{
@@ -36,7 +41,38 @@ func TestNoErrorWhenRequestNameNotPresent(t *testing.T) {
 		URL:   "http://nicholasjackson.io",
 	}
 
+	// err != nil: validate return ok
+	if err := validate.Struct(&request); err == nil {
+		t.Error(err)
+	}
+}
+
+// Case 4: Test return error if a name is present but a length is in range
+func TestNoErrorWhenRequestNamePresent(t *testing.T) {
+	validate := validator.New()
+	request := Request{
+		Name:  "Hua Van Thong",
+		Email: "myname@address.com",
+		URL:   "http://nicholasjackson.io",
+	}
+
+	// err != nil: validate return ok
 	if err := validate.Struct(&request); err != nil {
+		t.Error(err)
+	}
+}
+
+// Case 5: Test return error if a name is present but a length is greater 30
+func TestNoErrorWhenRequestNamePresentGreater(t *testing.T) {
+	validate := validator.New()
+	request := Request{
+		Name:  "Hua Van Thong Hackerrrrrrrrrrrrrrrrrrrrrr",
+		Email: "myname@address.com",
+		URL:   "http://nicholasjackson.io",
+	}
+
+	// err != nil: validate return ok
+	if err := validate.Struct(&request); err == nil {
 		t.Error(err)
 	}
 }
